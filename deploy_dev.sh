c01ab0b15fbbf8fa1efda8d6651c738b652d8e23
Kendrick Martin <kmartinix@gmail.com>
2018-09-05 18:13:50 -0700

    Adding updates to build process

--- deploy_dev.sh
+++ deploy_dev.sh
@@ -2,6 +2,7 @@
 
 IMAGE_NAME=portainer/agent:local
 LOG_LEVEL=DEBUG
+VAGRANT=true
 
 if [[ $# -ne 1 ]] ; then
   echo "Usage: $(basename $0) <VERSION>"
@@ -10,9 +11,16 @@ fi
 
 MODE=$1
 
+function compile() {
+  echo "Compilation..."
+  ./build_in_container.sh linux amd64
+}
+
+
 function deploy_local() {
   echo "Cleanup previous settings..."
-  docker rm -f pagent
+
+  docker rmi ${IMAGE_NAME}
 
   echo "Image build..."
   docker build --no-cache -t ${IMAGE_NAME} -f ../../Dockerfile ../..
@@ -29,50 +37,60 @@ function deploy_local() {
 }
 
 function deploy_swarm() {
-  echo "Cleanup previous settings..."
+  ##Assuming you're using the deviantony/vagrant-swarm-cluster.git setup
 
-  docker -H 10.0.7.10:2375 service rm pagent
-  docker -H 10.0.7.10:2375 network rm pagent-net
-  docker -H 10.0.7.10:2375 rmi -f ${IMAGE_NAME}
-  docker -H 10.0.7.11:2375 rmi -f ${IMAGE_NAME}
+  if [ ["$VAGRANT"] ]
+  then
+    echo "Cleanup previous settings..."
+    rm portainer-agent.tar
+    DOCKER_MANAGER=10.0.7.10
+    DOCKER_NODE=10.0.7.11
+    docker -H $DOCKER_MANAGER:2375 service rm pagent
+    docker -H $DOCKER_MANAGER:2375 network rm pagent-net
+    docker -H $DOCKER_MANAGER:2375 rmi -f ${IMAGE_NAME}
+    docker -H $DOCKER_NODE:2375 rmi -f ${IMAGE_NAME}
 
-  echo "Image build..."
+    echo "Image build..."
+    ##First we build locally
+    docker build --no-cache -t ${IMAGE_NAME} .
+    docker save ${IMAGE_NAME} -o portainer-agent.tar
+    docker -H $DOCKER_MANAGER:2375 load -i portainer-agent.tar
+    docker -H $DOCKER_NODE:2375 load -i portainer-agent.tar
 
-  docker -H 10.0.7.10:2375 build --no-cache -t ${IMAGE_NAME} -f ../../Dockerfile ../..
-  docker -H 10.0.7.11:2375 build --no-cache -t ${IMAGE_NAME} -f ../../Dockerfile ../..
+    echo "Sleep..."
+    sleep 5
+    
+    echo "Deployment..."
 
-  echo "Sleep..."
+    docker -H $DOCKER_MANAGER:2375 network create --driver overlay --attachable pagent-net
+    docker -H $DOCKER_MANAGER:2375 service create --name pagent \
+    --network pagent-net \
+    -e LOG_LEVEL=${LOG_LEVEL} \
+    -e AGENT_CLUSTER_ADDR=tasks.pagent \
+    --mode global \
+    --mount type=bind,src=//var/run/docker.sock,dst=/var/run/docker.sock \
+    --mount type=bind,src=//var/lib/docker/volumes,dst=/var/lib/docker/volumes \
+    --publish mode=host,target=9001,published=9001 \
+    --restart-condition none \
+    ${IMAGE_NAME}
 
-  sleep 3
-
-  echo "Deployment..."
-
-  docker -H 10.0.7.10:2375 network create --driver overlay --attachable pagent-net
-  docker -H 10.0.7.10:2375 service create --name pagent \
-  --network pagent-net \
-  -e LOG_LEVEL=${LOG_LEVEL} \
-  -e AGENT_CLUSTER_ADDR=tasks.pagent \
-  --mode global \
-  --mount type=bind,src=//var/run/docker.sock,dst=/var/run/docker.sock \
-  --mount type=bind,src=//var/lib/docker/volumes,dst=/var/lib/docker/volumes \
-  --publish mode=host,target=9001,published=9001 \
-  --restart-condition none \
-  ${IMAGE_NAME}
-
-  docker -H 10.0.7.10:2375 service logs -f pagent
+    docker -H $DOCKER_MANAGER:2375 service logs -f pagent
+  else
+    echo "Unsupported configuration"
+  fi
 }
 
 function main() {
 
-  mkdir dist
-  cd cmd/agent
+  # mkdir dist
+  # cd cmd/agent
+  #
 
-  echo "Compilation..."
-
-  CGO_ENABLED=0 go build -a --installsuffix cgo --ldflags '-s'
-  rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
-  mv agent ../../dist/agent
-
+  # echo "Compilation..."
+  # CGO_ENABLED=0 go build -a --installsuffix cgo --ldflags '-s'
+  # rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
+  # mv agent ../../dist/agent
+  compile
   if [ ${MODE} == 'local' ]
   then
     deploy_local
