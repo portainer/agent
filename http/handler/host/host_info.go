fde63768f71365971c2e9119b6c847bf9c4db781
Chaim Lev-Ari <chiptus@gmail.com>
2018-09-20 13:49:09 +0300

    feat(host-info): get a more complete host info

--- http/handler/host/host_info.go
+++ http/handler/host/host_info.go
@@ -3,21 +3,96 @@ package host
 import (
 	"net/http"
 
-	httperror "github.com/portainer/agent/http/error"
-	"github.com/portainer/agent/http/response"
+	"github.com/jaypipes/ghw"
+	"github.com/portainer/agent"
+	httperror "github.com/portainer/libhttp/error"
+	"github.com/portainer/libhttp/response"
 )
 
+type pciDevice struct {
+	Vendor       string
+	Version      string
+	SerialNumber string
+	Name         string
+}
+
+type physicalDisk struct {
+	Vendor string
+	Size   uint64
+}
+
 type hostInfo struct {
-	PhysicalDeviceVendor string
-	DeviceVersion        string
-	DeviceSerialNumber   string
-	InstalledPCIDevices  []string
-	PhysicalDisk         string
+	PCIDevices    []pciDevice
+	PhysicalDisks []physicalDisk
 }
 
 func (handler *Handler) hostInfo(rw http.ResponseWriter, r *http.Request) *httperror.HandlerError {
-	response.JSON(rw, hostInfo{
-		PhysicalDeviceVendor: "hello",
-	})
+	var hi hostInfo
+
+	err := fillHostInfo(&hi)
+	if err != nil {
+		return &httperror.HandlerError{http.StatusBadRequest, "Can't get host info", err}
+	}
+	response.JSON(rw, hi)
 	return nil
+}
+
+func fillHostInfo(hi *hostInfo) error {
+	devices, err := fillPciDevices()
+	if err != nil {
+		return err
+	}
+	hi.PCIDevices = devices
+
+	disks, err := fillPhysicalDisk()
+	if err != nil {
+		return err
+	}
+	hi.PhysicalDisks = disks
+	return nil
+
+}
+
+func fillPciDevices() ([]pciDevice, error) {
+	pci, err := ghw.PCI()
+	if err != nil {
+		return nil, err
+	}
+
+	devicesRaw := pci.ListDevices()
+
+	if len(devicesRaw) == 0 {
+		return nil, agent.Error("Could not retrieve PCI devices")
+	}
+
+	var devices []pciDevice
+	for _, device := range devicesRaw {
+		devices = append(devices, pciDevice{
+			Vendor: device.Vendor.Name,
+			// DeviceSerialNumber: device.
+			// DeviceVersion
+			Name: device.Product.Name,
+		})
+
+	}
+
+	return devices, nil
+}
+
+func fillPhysicalDisk() ([]physicalDisk, error) {
+	block, err := ghw.Block()
+	if err != nil {
+		return nil, err
+	}
+
+	var disks []physicalDisk
+
+	for _, disk := range block.Disks {
+		disks = append(disks, physicalDisk{
+			Vendor: disk.Vendor,
+			Size:   disk.SizeBytes,
+		})
+	}
+
+	return disks, nil
 }
