8498e37fafecb3c9399b179e0aa0d37e6fcbe1cb
Chaim Lev-Ari <chiptus@gmail.com>
2018-09-26 09:19:28 +0300

    feat(agent-proxy): move agent proxy to root handler

--- http/handler/browse/handler.go
+++ http/handler/browse/handler.go
@@ -3,31 +3,31 @@ package browse
 import (
 	"net/http"
 
-	"github.com/portainer/agent"
-	"github.com/portainer/agent/http/handler/agentproxy"
+	"github.com/gorilla/mux"
 	httperror "github.com/portainer/libhttp/error"
 )
 
 // Handler is the HTTP handler used to handle volume browsing operations.
 type Handler struct {
-	agentproxy.Handler
+	*mux.Router
 }
 
 // NewHandler returns a pointer to an Handler
 // It sets the associated handle functions for all the Browse related HTTP endpoints.
-func NewHandler(clusterService agent.ClusterService, agentTags map[string]string) *Handler {
-	h := &Handler{}
-	h.CreateAgentProxyHandler(clusterService, agentTags)
+func NewHandler(agentProxy func(next http.Handler) http.Handler) *Handler {
+	h := &Handler{
+		Router: mux.NewRouter(),
+	}
 
 	h.Handle("/browse/{id}/ls",
-		h.AgentProxy(httperror.LoggerHandler(h.browseList))).Methods(http.MethodGet)
+		agentProxy(httperror.LoggerHandler(h.browseList))).Methods(http.MethodGet)
 	h.Handle("/browse/{id}/get",
-		h.AgentProxy(httperror.LoggerHandler(h.browseGet))).Methods(http.MethodGet)
+		agentProxy(httperror.LoggerHandler(h.browseGet))).Methods(http.MethodGet)
 	h.Handle("/browse/{id}/delete",
-		h.AgentProxy(httperror.LoggerHandler(h.browseDelete))).Methods(http.MethodDelete)
+		agentProxy(httperror.LoggerHandler(h.browseDelete))).Methods(http.MethodDelete)
 	h.Handle("/browse/{id}/rename",
-		h.AgentProxy(httperror.LoggerHandler(h.browseRename))).Methods(http.MethodPut)
+		agentProxy(httperror.LoggerHandler(h.browseRename))).Methods(http.MethodPut)
 	h.Handle("/browse/{id}/put",
-		h.AgentProxy(httperror.LoggerHandler(h.browsePut))).Methods(http.MethodPost)
+		agentProxy(httperror.LoggerHandler(h.browsePut))).Methods(http.MethodPost)
 	return h
 }
--- http/handler/host/handler.go
+++ http/handler/host/handler.go
@@ -3,26 +3,27 @@ package host
 import (
 	"net/http"
 
+	"github.com/gorilla/mux"
+
 	"github.com/portainer/agent"
-	"github.com/portainer/agent/http/handler/agentproxy"
 	httperror "github.com/portainer/libhttp/error"
 )
 
 // Handler represents an HTTP API Handler for host specific actions
 type Handler struct {
-	agentproxy.Handler
+	*mux.Router
 	systemService agent.SystemService
 }
 
 // NewHandler returns a new instance of Handler
-func NewHandler(systemService agent.SystemService, clusterService agent.ClusterService, agentTags map[string]string) *Handler {
+func NewHandler(systemService agent.SystemService, agentProxy func(http.Handler) http.Handler) *Handler {
 	h := &Handler{
+		Router:        mux.NewRouter(),
 		systemService: systemService,
 	}
-	h.CreateAgentProxyHandler(clusterService, agentTags)
 
 	h.Handle("/host/info",
-		h.AgentProxy(httperror.LoggerHandler(h.hostInfo))).Methods(http.MethodGet)
+		agentProxy(httperror.LoggerHandler(h.hostInfo))).Methods(http.MethodGet)
 
 	return h
 }
