0c6fc86bcfebb7e8fa7090a3885e033d2eb70de0
Chaim Lev-Ari <chiptus@gmail.com>
2018-11-11 10:27:29 +0200

    fix(system): set host root on system service init

--- ghw/system.go
+++ ghw/system.go
@@ -1,10 +1,16 @@
 package ghw
 
+import "os"
+
 // SystemService is used to get info about the host
 type SystemService struct {
 }
 
 // NewSystemService returns a pointer to a new SystemService
-func NewSystemService() *SystemService {
+func NewSystemService(hostRoot string) *SystemService {
+	// TODO should be passed to `PCI()` instead of setting the envvar
+	// TODO should we clean this var?
+	os.Setenv("PCIDB_CHROOT", hostRoot)
+	os.Setenv("GHW_CHROOT", hostRoot)
 	return &SystemService{}
 }
