e71326592468532e141d6ea406584586699ba318
Chaim Lev-Ari <chiptus@gmail.com>
2018-11-15 18:11:37 +0200

    fix(system): remove pcidb env var

--- ghw/system.go
+++ ghw/system.go
@@ -1,7 +1,5 @@
 package ghw
 
-import "os"
-
 // SystemService is used to get info about the host
 type SystemService struct {
 	hostRoot string
@@ -9,9 +7,6 @@ type SystemService struct {
 
 // NewSystemService returns a pointer to a new SystemService
 func NewSystemService(hostRoot string) *SystemService {
-	// TODO should be passed to `PCI()` instead of setting the envvar
-	// TODO should we clean this var?
-	os.Setenv("PCIDB_CHROOT", hostRoot)
 	service := &SystemService{}
 	service.hostRoot = hostRoot
 	return service
