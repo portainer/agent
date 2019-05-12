3e00d9a6e9d4bcd59f230bfba16358a8c522f0c9
Anthony Lapenna <lapenna.anthony@gmail.com>
2019-05-13 09:45:41 +1200

    feat(http): add support for websocket attach

---
+++ http/handler/websocket/hijack.go
@@ -0,0 +1,37 @@
+package websocket
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httputil"
+
+	"github.com/gorilla/websocket"
+)
+
+func hijackRequest(websocketConn *websocket.Conn, httpConn *httputil.ClientConn, request *http.Request) error {
+	// Server hijacks the connection, error 'connection closed' expected
+	resp, err := httpConn.Do(request)
+	if err != httputil.ErrPersistEOF {
+		if err != nil {
+			return err
+		}
+		if resp.StatusCode != http.StatusSwitchingProtocols {
+			resp.Body.Close()
+			return fmt.Errorf("unable to upgrade to tcp, received %d", resp.StatusCode)
+		}
+	}
+
+	tcpConn, brw := httpConn.Hijack()
+	defer tcpConn.Close()
+
+	errorChan := make(chan error, 1)
+	go streamFromTCPConnToWebsocketConn(websocketConn, brw, errorChan)
+	go streamFromWebsocketConnToTCPConn(websocketConn, tcpConn, errorChan)
+
+	err = <-errorChan
+	if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseNoStatusReceived) {
+		return err
+	}
+
+	return nil
+}
