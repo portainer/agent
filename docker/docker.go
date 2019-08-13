f311d0107719da015bea12682a0cd340a0962d79
Anthony Lapenna <lapenna.anthony@gmail.com>
2019-08-13 11:41:28 +0200

    feat(docker): enhance valid network detection when retrieving container IP

--- docker/docker.go
+++ docker/docker.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"log"
 
+	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/client"
 	"github.com/portainer/agent"
 )
@@ -62,9 +63,19 @@ func (service *InfoService) GetContainerIpFromDockerEngine(containerName string)
 		log.Printf("[WARN] [docker] [network_count: %d] [message: Agent container running in more than a single Docker network. This might cause communication issues]", len(containerInspect.NetworkSettings.Networks))
 	}
 
-	for name, network := range containerInspect.NetworkSettings.Networks {
-		if name != "ingress" && network.IPAddress != "" {
-			log.Printf("[DEBUG] [docker] [ip_address: %s] [message: Retrieving IP address from container networks]", network.IPAddress)
+	for networkName, network := range containerInspect.NetworkSettings.Networks {
+		networkInspect, err := cli.NetworkInspect(context.Background(), network.NetworkID, types.NetworkInspectOptions{})
+		if err != nil {
+			return "", err
+		}
+
+		if networkInspect.Ingress || networkInspect.Scope != "swarm" {
+			log.Printf("[DEBUG] [docker] [network_name: %s] [scope: %s] [ingress: %t] [message: Skipping invalid container network]", networkInspect.Name, networkInspect.Scope, networkInspect.Ingress)
+			continue
+		}
+
+		if network.IPAddress != "" {
+			log.Printf("[DEBUG] [docker] [ip_address: %s] [network_name: %s] [message: Retrieving IP address from container network]", network.IPAddress, networkName)
 			return network.IPAddress, nil
 		}
 	}
