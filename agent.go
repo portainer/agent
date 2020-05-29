ef30e131cd9b546d114196db86585cd15c58e07d
Anthony Lapenna <lapenna.anthony@gmail.com>
2020-05-29 14:40:56 +1200

    chore(version): bump version number

--- agent.go
+++ agent.go
@@ -154,7 +154,7 @@ type (
 
 const (
 	// Version represents the version of the agent.
-	Version = "1.5.1"
+	Version = "1.6.0"
 	// APIVersion represents the version of the agent's API.
 	APIVersion = "2"
 	// DefaultAgentAddr is the default address used by the Agent API server.
