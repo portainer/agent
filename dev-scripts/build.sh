0501308020d4eb9f0a14f6a6ea5cc82dd6e041e8
Chaim Lev-Ari <chiptus@gmail.com>
2021-02-17 10:52:02 +0200

    chore(dev): create new dev script (WIP)

---
+++ dev-scripts/build.sh
@@ -0,0 +1,14 @@
+
+function build() {
+  local ret_value=""
+  default_image_name
+  DEFAULT_IMAGE_NAME=$ret_value
+
+  local IMAGE_NAME="${1:-$DEFAULT_IMAGE_NAME}"
+  docker rmi -f $IMAGE_NAME || true
+
+  msg "Image build..."
+  docker build --no-cache -t $IMAGE_NAME -f build/linux/Dockerfile .
+
+  msg "Image $IMAGE_NAME is built"
+}
---
+++ dev-scripts/compile.sh
@@ -0,0 +1,13 @@
+function compile() {
+  TARGET_DIST=dist/agent
+
+  msg "Compilation..."
+
+  cd cmd/agent
+  GOOS="linux" GOARCH="amd64" CGO_ENABLED=0 go build -a --installsuffix cgo --ldflags '-s'
+  rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
+  cd ../..
+  mv cmd/agent/agent $TARGET_DIST
+
+  msg "Agent executable is available on $TARGET_DIST"
+}
