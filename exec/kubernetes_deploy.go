da73352dda608dbba76a8bd75af001ea22d4b7c4
Simon Meng <simon.meng@portainer.io>
2021-02-17 15:34:57 +1300

    feat(http): run kubectl without parameters of token and insecure-skip-tls-verify (GH#170)

--- exec/kubernetes_deploy.go
+++ exec/kubernetes_deploy.go
@@ -3,7 +3,6 @@ package exec
 import (
 	"bytes"
 	"fmt"
-	"io/ioutil"
 	"os/exec"
 	"path"
 	"runtime"
@@ -24,20 +23,16 @@ func NewKubernetesDeployer(binaryPath string) *KubernetesDeployer {
 
 // Deploy will deploy a Kubernetes manifest inside a specific namespace
 // it will use kubectl to deploy the manifest.
+// kubectl uses in-cluster config.
 func (deployer *KubernetesDeployer) Deploy(data string, namespace string) ([]byte, error) {
-	token, err := ioutil.ReadFile("/var/run/secrets/kubernetes.io/serviceaccount/token")
-	if err != nil {
-		return nil, err
-	}
-
 	command := path.Join(deployer.binaryPath, "kubectl")
 	if runtime.GOOS == "windows" {
 		command = path.Join(deployer.binaryPath, "kubectl.exe")
 	}
 
 	args := make([]string, 0)
-	args = append(args, "--insecure-skip-tls-verify")
-	args = append(args, "--token", string(token))
+	// Specifying "--insecure-skip-tls-verify" make kubectl return error "default cluster has no server defined"
+	//args = append(args, "--insecure-skip-tls-verify")
 	args = append(args, "--namespace", namespace)
 	args = append(args, "apply", "-f", "-")
 
