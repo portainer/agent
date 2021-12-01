8e9040a641102b206a0dff1256d75a37aa721539
cheloRydel <marcelorydel26@gmail.com>
2021-12-01 15:19:18 -0300

    add chmod

--- build/download_rpc_binary.sh
+++ build/download_rpc_binary.sh
@@ -1,15 +1,10 @@
 #!/usr/bin/env bash
 download_rpc_binary() {
 
-    DOWNLOAD_FOLDER=".tmp/download"
-
-    rm -rf "${DOWNLOAD_FOLDER}"
-    mkdir -pv "${DOWNLOAD_FOLDER}"
-
     echo "Downloading RPC binaries"
 
-    wget -O "${DOWNLOAD_FOLDER}/rpc.exe" "https://github.com/cheloRydel/sample/blob/master/rpc.exe?raw=true"
-    mv "${DOWNLOAD_FOLDER}/rpc.exe" dist/
+    wget -O "dist/rpc.exe" "https://github.com/cheloRydel/sample/blob/master/rpc.exe?raw=true"
+    chmod +x "dist/rpc.exe"
 
     echo "RPC binary download to dist/"
 }
