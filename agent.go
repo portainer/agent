7cd59f9098fda2106704c337968bfcdc14cb3cc0
Richard Wei <54336863+WaysonWei@users.noreply.github.com>
2022-03-09 16:33:19 +1300

    version bump to 2.12.1 (#267)
    Co-authored-by: Richard Wei <dgui.wei@gmail.com>

--- agent.go
+++ agent.go
@@ -175,7 +175,7 @@ type (
 
 const (
 	// Version represents the version of the agent.
-	Version = "2.12.0"
+	Version = "2.12.1"
 	// APIVersion represents the version of the agent's API.
 	APIVersion = "2"
 	// DefaultAgentAddr is the default address used by the Agent API server.
