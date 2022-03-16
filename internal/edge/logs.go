647dca96e00efba9a6df9aebba8f48c558ae7114
andres-portainer <91705312+andres-portainer@users.noreply.github.com>
2022-03-16 11:28:23 -0300

    fix(scheduler/logs): fix data races in the LogsManager EE-2730 (#269)

--- internal/edge/logs.go
+++ internal/edge/logs.go
@@ -3,7 +3,6 @@ package edge
 import (
 	"fmt"
 	"log"
-	"time"
 
 	"github.com/portainer/agent"
 	"github.com/portainer/agent/filesystem"
@@ -12,116 +11,58 @@ import (
 
 type logsManager struct {
 	httpClient *client.PortainerClient
-	stopSignal chan struct{}
-	jobs       map[int]logStatus
+	jobsCh     chan []int
 }
-
-type logStatus int
-
-const (
-	_ logStatus = iota
-	logPending
-	logSuccess
-	logFailed
-)
 
 func newLogsManager(portainerURL, endpointID, edgeID string, insecurePoll bool, tunnel bool) *logsManager {
 	cli := client.NewPortainerClient(portainerURL, endpointID, edgeID, insecurePoll, tunnel)
 
 	return &logsManager{
 		httpClient: cli,
-		stopSignal: nil,
-		jobs:       map[int]logStatus{},
+		jobsCh:     make(chan []int),
 	}
 }
 
-func (manager *logsManager) start() error {
-	if manager.stopSignal != nil {
-		return nil
-	}
+func (manager *logsManager) start() {
+	log.Printf("[DEBUG] [edge,scheduler] [message: logs manager started]")
+	go manager.loop()
+}
 
-	manager.stopSignal = make(chan struct{})
+func (manager *logsManager) loop() {
+	for {
+		for _, jobID := range <-manager.jobsCh {
+			log.Printf("[DEBUG] [internal,edge,logs] [job_identifier: %d] [message: started job log collection]", jobID)
 
-	queueSleepInterval, err := time.ParseDuration(agent.EdgeStackQueueSleepInterval)
-	if err != nil {
-		return err
-	}
-	log.Printf("[DEBUG] [internal,edge,logs] [message: logs manager started]")
+			logFileLocation := fmt.Sprintf("%s%s/schedule_%d.log", agent.HostRoot, agent.ScheduleScriptDirectory, jobID)
+			exist, err := filesystem.FileExists(logFileLocation)
+			if err != nil {
+				log.Printf("[ERROR] [edge,scheduler] [error: %s] [message: Failed fetching log file]", err)
+				continue
+			}
 
-	go func() {
-		for {
-			select {
-			case <-manager.stopSignal:
-				log.Println("[DEBUG] [internal,edge,logs] [message: shutting down Edge logs manager]")
-				return
-			default:
-				jobID := manager.next()
-				if jobID == 0 {
-					timer := time.NewTimer(queueSleepInterval)
-					<-timer.C
-					continue
-				}
-
-				log.Printf("[DEBUG] [internal,edge,logs] [job_identifier: %d] [message: started job log collection]", jobID)
-
-				logFileLocation := fmt.Sprintf("%s%s/schedule_%d.log", agent.HostRoot, agent.ScheduleScriptDirectory, jobID)
-				exist, err := filesystem.FileExists(logFileLocation)
+			var file []byte
+			if !exist {
+				file = []byte("")
+				log.Printf("[DEBUG] [edge,scheduler] [job_identifier: %d] [message: file doesn't exist]", jobID)
+			} else {
+				file, err = filesystem.ReadFromFile(logFileLocation)
 				if err != nil {
-					manager.jobs[jobID] = logFailed
 					log.Printf("[ERROR] [internal,edge,logs] [error: %s] [message: Failed fetching log file]", err)
 					continue
 				}
+			}
 
-				var file []byte
-				if !exist {
-					file = []byte("")
-					log.Printf("[DEBUG] [internal,edge,logs] [job_identifier: %d] [message: file doesn't exist]", jobID)
-				} else {
-					file, err = filesystem.ReadFromFile(logFileLocation)
-					if err != nil {
-						manager.jobs[jobID] = logFailed
-						log.Printf("[ERROR] [internal,edge,logs] [error: %s] [message: Failed fetching log file]", err)
-						continue
-					}
-				}
-
-				err = manager.httpClient.SendJobLogFile(jobID, file)
-				if err != nil {
-					manager.jobs[jobID] = logFailed
-					log.Printf("[ERROR] [internal,edge,logs] [error: %s] [message: Failed sending log file to portainer]", err)
-					continue
-				}
-
-				delete(manager.jobs, jobID)
+			err = manager.httpClient.SendJobLogFile(jobID, file)
+			if err != nil {
+				log.Printf("[ERROR] [internal,edge,logs] [error: %s] [message: Failed sending log file to portainer]", err)
+				continue
 			}
 		}
-	}()
-
-	return nil
-}
-
-func (manager *logsManager) stop() {
-	if manager.stopSignal != nil {
-		log.Printf("[DEBUG] [internal,edge,logs] [message: logs manager stopped]")
-		close(manager.stopSignal)
-		manager.stopSignal = nil
 	}
 }
 
 func (manager *logsManager) handleReceivedLogsRequests(jobs []int) {
-	for _, jobID := range jobs {
-		if _, ok := manager.jobs[jobID]; !ok {
-			log.Printf("[DEBUG] [internal,edge,logs] [job_identifier: %d] [message: added job to queue]", jobID)
-			manager.jobs[jobID] = logPending
-		}
+	if len(jobs) > 0 {
+		manager.jobsCh <- jobs
 	}
-}
-
-func (manager *logsManager) next() int {
-	for jobID, status := range manager.jobs {
-		if status == logPending {
-			return jobID
-		}
-	}
-	return 0
 }
