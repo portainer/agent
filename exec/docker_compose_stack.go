502315991c1ed9b4feb4f64df56ac47f79235821
Matt Hook <hookenz@gmail.com>
2022-03-25 12:38:54 +1300

    Remove redundant login/logout stubs that were not being called and will now never be used

--- exec/docker_compose_stack.go
+++ exec/docker_compose_stack.go
@@ -29,18 +29,6 @@ func NewDockerComposeStackService(binaryPath string) (*DockerComposeStackService
 	return service, nil
 }
 
-// Login executes the docker login command against a list of registries (including DockerHub).
-func (service *DockerComposeStackService) Login() error {
-	// Not implemented yet.
-	return nil
-}
-
-// Logout executes the docker logout command.
-func (service *DockerComposeStackService) Logout() error {
-	return nil
-
-}
-
 // Deploy executes the docker stack deploy command.
 func (service *DockerComposeStackService) Deploy(ctx context.Context, name string, filePaths []string, prune bool) error {
 	return service.deployer.Deploy(ctx, "", "", name, filePaths, "")
