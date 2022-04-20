46536dd5ac9873c88289d0509b824123f892e966
Matt Hook <hookenz@gmail.com>
2022-04-20 17:18:50 +1200

    bump version (#298)

--- agent.go
+++ agent.go
@@ -196,7 +196,7 @@ type (
 
 const (
 	// Version represents the version of the agent.
-	Version = "2.12.0"
+	Version = "2.13.0"
 	// APIVersion represents the version of the agent's API.
 	APIVersion = "2"
 	// DefaultAgentAddr is the default address used by the Agent API server.
