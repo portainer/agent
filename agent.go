8c9fe4a73ff29ff978cb2f390cbd53dec41bedf2
Prabhat Khera <91852476+prabhat-org@users.noreply.github.com>
2022-05-11 13:48:44 +1200

    bump version to 2.13.1 (#310)

--- agent.go
+++ agent.go
@@ -204,7 +204,7 @@ type (
 
 const (
 	// Version represents the version of the agent.
-	Version = "2.13.0"
+	Version = "2.13.1"
 	// APIVersion represents the version of the agent's API.
 	APIVersion = "2"
 	// DefaultAgentAddr is the default address used by the Agent API server.
