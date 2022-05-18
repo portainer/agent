9644b6f1a6c3a2af5019310fbbf335dea0828f14
oscarzhou <zhouhongyu1989@gmail.com>
2022-05-19 08:51:32 +1200

    feat(nomad): add reverse proxy transport for http back

--- http/proxy/nomad.go
+++ http/proxy/nomad.go
@@ -17,32 +17,40 @@ func NewNomadProxy(nomadConfig agent.NomadConfig) http.Handler {
 
 	proxy := httputil.NewSingleHostReverseProxy(remoteURL)
 
-	if nomadConfig.NomadClientCert != "" && nomadConfig.NomadClientKey != "" {
-		var caCertPool *x509.CertPool
-		// Create a CA certificate pool and add cert.pem to it
-		if nomadConfig.NomadCACert != "" {
-			caCert, err := ioutil.ReadFile(nomadConfig.NomadCACert)
-			if err != nil {
-				log.Fatalf("[ERROR] [proxy,nomad] [message: failed to read Nomad CA Cert]")
+	if nomadConfig.NomadTLSEnabled {
+		if nomadConfig.NomadClientCert != "" && nomadConfig.NomadClientKey != "" {
+			var caCertPool *x509.CertPool
+			// Create a CA certificate pool and add cert.pem to it
+			if nomadConfig.NomadCACert != "" {
+				caCert, err := ioutil.ReadFile(nomadConfig.NomadCACert)
+				if err != nil {
+					log.Fatalf("[ERROR] [proxy,nomad] [message: failed to read Nomad CA Cert]")
+				}
+				caCertPool = x509.NewCertPool()
+				caCertPool.AppendCertsFromPEM(caCert)
 			}
-			caCertPool = x509.NewCertPool()
-			caCertPool.AppendCertsFromPEM(caCert)
-		}
 
-		// Create an HTTPS client and supply the created CA pool and certificate
+			// Create an HTTPS client and supply the created CA pool and certificate
+			proxy.Transport = &http.Transport{
+				TLSClientConfig: &tls.Config{
+					RootCAs:    caCertPool,
+					MinVersion: tls.VersionTLS13,
+					MaxVersion: tls.VersionTLS13,
+					GetClientCertificate: func(chi *tls.CertificateRequestInfo) (*tls.Certificate, error) {
+						cert, err := tls.LoadX509KeyPair(nomadConfig.NomadClientCert, nomadConfig.NomadClientKey)
+						if err != nil {
+							return nil, err
+						}
+
+						return &cert, nil
+					},
+				},
+			}
+		}
+	} else {
 		proxy.Transport = &http.Transport{
 			TLSClientConfig: &tls.Config{
-				RootCAs:    caCertPool,
-				MinVersion: tls.VersionTLS13,
-				MaxVersion: tls.VersionTLS13,
-				GetClientCertificate: func(chi *tls.CertificateRequestInfo) (*tls.Certificate, error) {
-					cert, err := tls.LoadX509KeyPair(nomadConfig.NomadClientCert, nomadConfig.NomadClientKey)
-					if err != nil {
-						return nil, err
-					}
-
-					return &cert, nil
-				},
+				InsecureSkipVerify: true,
 			},
 		}
 	}
