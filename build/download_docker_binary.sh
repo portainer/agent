017cc3ecb85173a2028b22fd230fe4bd0dfa1096
Matt Hook <hookenz@gmail.com>
2022-05-27 15:56:03 +1200

    use compose v2 and scripts from other places

--- build/download_docker_binary.sh
+++ build/download_docker_binary.sh
@@ -1,44 +1,35 @@
 #!/usr/bin/env bash
-download_docker_binary() {
-    local PLATFORM=$1
-    local ARCH=$2
-    
-    local DOCKER_VERSION=$3
-    
-    DOWNLOAD_FOLDER=".tmp/download"
-    
-    if [ "$PLATFORM" = "windows" ]; then
-        PLATFORM="win"
-    fi
-    
-    if [ "$ARCH" = "amd64" ]; then
-        ARCH="x86_64"
-    fi
+set -euo pipefail
 
-    if [ "$ARCH" = "arm" ]; then
-        ARCH="armhf"
-    fi
+if [[ $# -ne 3 ]]; then
+    echo "Illegal number of parameters" >&2
+    exit 1
+fi
 
-    if [ "$ARCH" = "arm64" ]; then
-        ARCH="aarch64"
-    fi
+PLATFORM=$1
+ARCH=$2
+DOCKER_VERSION=${3:1}
+DOWNLOAD_FOLDER=".tmp/download"
 
-    
-    rm -rf "${DOWNLOAD_FOLDER}"
-    mkdir -pv "${DOWNLOAD_FOLDER}"
-    
-    echo "Downloading docker binaries for ${PLATFORM} ${ARCH}"
-    
-    if [ "${PLATFORM}" == 'win' ]; then
-        wget -O "${DOWNLOAD_FOLDER}/docker-binaries.zip" "https://dockermsft.azureedge.net/dockercontainer/docker-${DOCKER_VERSION}.zip"
-        unzip "${DOWNLOAD_FOLDER}/docker-binaries.zip" -d "${DOWNLOAD_FOLDER}"
-        mv "${DOWNLOAD_FOLDER}/docker/docker.exe" dist/
-        mv ${DOWNLOAD_FOLDER}/docker/*.dll dist/
-    else
-        wget -O "${DOWNLOAD_FOLDER}/docker-binaries.tgz" "https://download.docker.com/${PLATFORM}/static/stable/${ARCH}/docker-${DOCKER_VERSION}.tgz"
-        tar -xf "${DOWNLOAD_FOLDER}/docker-binaries.tgz" -C "${DOWNLOAD_FOLDER}"
-        mv "${DOWNLOAD_FOLDER}/docker/docker" dist/
-    fi
-    
-    echo "Docker binary download to dist/"
-}
+
+if [[ ${ARCH} == "amd64" ]]; then
+    ARCH="x86_64"
+elif [[ ${ARCH} == "arm" ]]; then
+    ARCH="armhf"
+elif [[ ${ARCH} == "arm64" ]]; then
+    ARCH="aarch64"
+fi
+
+rm -rf "${DOWNLOAD_FOLDER}"
+mkdir -pv "${DOWNLOAD_FOLDER}"
+
+if [[ ${PLATFORM} == "windows" ]]; then
+    PLATFORM="win"
+    wget -O "${DOWNLOAD_FOLDER}/docker-binaries.zip" "https://download.docker.com/${PLATFORM}/static/stable/${ARCH}/docker-${DOCKER_VERSION}.zip"
+    unzip "${DOWNLOAD_FOLDER}/docker-binaries.zip" -d "${DOWNLOAD_FOLDER}"
+    mv "${DOWNLOAD_FOLDER}/docker/docker.exe" dist/
+else
+    wget -O "${DOWNLOAD_FOLDER}/docker-binaries.tgz" "https://download.docker.com/${PLATFORM}/static/stable/${ARCH}/docker-${DOCKER_VERSION}.tgz"
+    tar -xf "${DOWNLOAD_FOLDER}/docker-binaries.tgz" -C "${DOWNLOAD_FOLDER}"
+    mv "${DOWNLOAD_FOLDER}/docker/docker" dist/
+fi
--- build/download_docker_compose_binary.sh
+++ build/download_docker_compose_binary.sh
@@ -1,52 +1,27 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -euo pipefail
-IFS=$'\n\t'
+
+if [[ $# -ne 3 ]]; then
+    echo "Illegal number of parameters" >&2
+    exit 1
+fi
+
+PLATFORM=$1
+ARCH=$2
+COMPOSE_VERSION=$3
 
 
-function download_docker_compose_binary() {
-    local PLATFORM=$1
-    local ARCH=$2
-    local BINARY_VERSION=$3
-    
-    if [ "$ARCH" = "x86_64" ]; then
-        ARCH="amd64"
-    fi
-    
-    if [ "${PLATFORM}" == 'linux' ] && [ "${ARCH}" == 'amd64' ]; then
-        wget -O "dist/docker-compose" "https://github.com/portainer/docker-compose-linux-amd64-static-binary/releases/download/${BINARY_VERSION}/docker-compose"
-        chmod +x "dist/docker-compose"
-        return
-    fi
-    
-    if [ "${PLATFORM}" == 'darwin' ]; then
-        wget -O "dist/docker-compose" "https://github.com/docker/compose/releases/download/${BINARY_VERSION}/docker-compose-Darwin-x86_64"
-        chmod +x "dist/docker-compose"
-        return
-    fi
-    
-    if [ "${PLATFORM}" == 'windows' ]; then
-        wget -O "dist/docker-compose.exe" "https://github.com/docker/compose/releases/download/${BINARY_VERSION}/docker-compose-Windows-x86_64.exe"
-        chmod +x "dist/docker-compose.exe"
-        return
-    fi
-}
+if [[ ${ARCH} == "amd64" ]]; then
+    ARCH="x86_64"
+elif [[ ${ARCH} == "arm" ]]; then
+    ARCH="armv7"
+fi
 
-function download_docker_compose_plugin() {
-    local PLATFORM=$1
-    local ARCH=$2
-    local PLUGIN_VERSION=$3
-       
-    if [ "$ARCH" = "arm" ]; then
-        ARCH="armv7"
-    fi
-    
-    FILENAME="docker-compose-${PLATFORM}-${ARCH}"
-    TARGET_FILENAME="docker-compose.plugin"
-    if [[ "$PLATFORM" == "windows" ]]; then
-        FILENAME="$FILENAME.exe"
-        TARGET_FILENAME="$TARGET_FILENAME.exe"
-    fi
-    
-    wget -O "dist/$TARGET_FILENAME" "https://github.com/docker/compose-cli/releases/download/v$PLUGIN_VERSION/$FILENAME"
-    chmod +x "dist/$TARGET_FILENAME"
-}
+if [[ "$PLATFORM" == "windows" ]]; then
+    wget -O "dist/docker-compose.exe" "https://github.com/docker/compose/releases/download/$COMPOSE_VERSION/docker-compose-windows-${ARCH}.exe"
+    chmod +x "dist/docker-compose.exe"
+else
+    wget -O "dist/docker-compose.plugin" "https://github.com/docker/compose/releases/download/$COMPOSE_VERSION/docker-compose-${PLATFORM}-${ARCH}"
+    chmod +x "dist/docker-compose.plugin"
+fi
+
