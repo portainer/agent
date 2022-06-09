81e316781a6feb9eb26636f9462ae920b8cb1974
Chaim Lev-Ari <chiptus@gmail.com>
2022-06-09 09:30:28 +0300

    fix(edge): send endpoint id on async poll

--- edge/client/portainer_edge_async_client.go
+++ edge/client/portainer_edge_async_client.go
@@ -56,8 +56,9 @@ func (client *PortainerAsyncClient) SetTimeout(t time.Duration) {
 }
 
 type AsyncRequest struct {
-	CommandTimestamp *time.Time `json:"commandTimestamp,omitempty"`
-	Snapshot         *snapshot  `json:"snapshot,omitempty"`
+	CommandTimestamp *time.Time           `json:"commandTimestamp,omitempty"`
+	Snapshot         *snapshot            `json:"snapshot,omitempty"`
+	EndpointId       portainer.EndpointID `json:"endpointId,omitempty"`
 }
 
 type snapshot struct {
@@ -113,6 +114,7 @@ func (client *PortainerAsyncClient) GetEnvironmentStatus(flags ...string) (*Poll
 	pollURL := fmt.Sprintf("%s/api/endpoints/edge/async", client.serverAddress)
 
 	payload := AsyncRequest{}
+	payload.EndpointId = client.getEndpointIDFn()
 
 	var doSnapshot, doCommand bool
 	for _, f := range flags {
