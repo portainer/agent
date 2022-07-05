25ba55c3c82d8aff8c0780a7e2bfe80d5b2f15ff
LP B <xAt0mZ@users.noreply.github.com>
2022-07-05 19:07:19 +0200

    fix(tls): downgrade min used TLS version to 1.2 (#344)

--- crypto/tls.go
+++ crypto/tls.go
@@ -3,6 +3,7 @@ package crypto
 import (
 	"crypto/rand"
 	"crypto/rsa"
+	"crypto/tls"
 	"crypto/x509"
 	"encoding/pem"
 	"math/big"
@@ -12,6 +13,18 @@ import (
 
 	"github.com/portainer/agent"
 )
+
+var TLS12CipherSuites = []uint16{
+	tls.TLS_AES_128_GCM_SHA256,
+	tls.TLS_AES_256_GCM_SHA384,
+	tls.TLS_CHACHA20_POLY1305_SHA256,
+	tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+	tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+	tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+	tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+	tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
+	tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
+}
 
 // TLSService is a service used to generate TLS cert and key files
 // to setup HTTPS.
