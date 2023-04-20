ebe2f396d475675165fb43dc552397a511f365ed
Matt Hook <hookenz@gmail.com>
2023-04-20 13:32:10 +1200

    feat(packages): upgrade packages [EE-5146] (#443)
    * update packages, make compose match portainer version
    
    * update to match server

--- setup.sh
+++ setup.sh
@@ -6,13 +6,11 @@ PLATFORM=${1:-"linux"}
 ARCH=${2:-"amd64"}
 
 DOCKER_VERSION="v20.10.21"
-DOCKER_COMPOSE_VERSION="v2.13.0"
-KUBECTL_VERSION="v1.24.1"
+DOCKER_COMPOSE_VERSION="v2.17.2"
+KUBECTL_VERSION="v1.26.3"
 
 mkdir -p dist/
 
 /usr/bin/env bash ./build/download_docker_binary.sh "$PLATFORM" "$ARCH" "$DOCKER_VERSION"
 /usr/bin/env bash ./build/download_docker_compose_binary.sh "$PLATFORM" "$ARCH" "$DOCKER_COMPOSE_VERSION"
 /usr/bin/env bash ./build/download_kubectl_binary.sh "$PLATFORM" "$ARCH" "$KUBECTL_VERSION"
-
-
