39c0dd394c63ca45ae96a67cc2adab1dcf06cf95
stevensbkang <skan070@gmail.com>
2023-10-05 12:57:37 +0900

    added missing mingit download

--- setup.sh
+++ setup.sh
@@ -5,17 +5,18 @@ IFS=$'\n\t'
 PLATFORM=${1:-"linux"}
 ARCH=${2:-"amd64"}
 
-
 BINARY_VERSION_FILE="./binary-version.json"
 
 dockerVersion=$(jq -r '.docker' < "${BINARY_VERSION_FILE}")
 dockerComposeVersion=$(jq -r '.dockerCompose' < "${BINARY_VERSION_FILE}")
 kubectlVersion=$(jq -r '.kubectl' < "${BINARY_VERSION_FILE}")
+mingitVersion=$(jq -r '.mingit' < "${BINARY_VERSION_FILE}")
 
-echo "Downloading binaries for docker ${dockerVersion}, docker-compose ${dockerComposeVersion}, kubectl ${kubectlVersion}"
+echo "Downloading binaries for docker ${dockerVersion}, docker-compose ${dockerComposeVersion}, kubectl ${kubectlVersion}, and mingit ${mingitVersion}"
 
 mkdir -p dist/
 
 /usr/bin/env bash ./build/download_docker_binary.sh "$PLATFORM" "$ARCH" "$dockerVersion"
 /usr/bin/env bash ./build/download_docker_compose_binary.sh "$PLATFORM" "$ARCH" "$dockerComposeVersion"
 /usr/bin/env bash ./build/download_kubectl_binary.sh "$PLATFORM" "$ARCH" "$kubectlVersion"
+/usr/bin/env bash ./build/download_mingit_binary.sh "$PLATFORM" "$ARCH" "$mingitVersion"
