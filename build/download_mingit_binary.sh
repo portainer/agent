2d9bacb6e5692ef4cc2dc1047b4fec482ae0e270
andres-portainer <91705312+andres-portainer@users.noreply.github.com>
2024-04-18 15:27:52 -0300

    fix(mingit): upgrade to v2.44.0.1 EE-7023 (#640)

--- build/download_mingit_binary.sh
+++ build/download_mingit_binary.sh
@@ -14,6 +14,6 @@ if [[ ${PLATFORM} == "windows" ]]; then
   GIT_VERSION=$(echo $MINGIT_VERSION | cut -d "." -f 1-3)
   GIT_PATCH_VERSION=$(echo $MINGIT_VERSION | cut -d "." -f 4)
 
-  wget https://github.com/git-for-windows/git/releases/download/v$GIT_VERSION.windows.$GIT_PATCH_VERSION/MinGit-$GIT_VERSION.$GIT_PATCH_VERSION-busybox-64-bit.zip
-  unzip MinGit-$GIT_VERSION.$GIT_PATCH_VERSION-busybox-64-bit.zip -d dist/mingit
+  wget https://github.com/git-for-windows/git/releases/download/v$GIT_VERSION.windows.$GIT_PATCH_VERSION/MinGit-$GIT_VERSION-busybox-64-bit.zip
+  unzip MinGit-$GIT_VERSION-busybox-64-bit.zip -d dist/mingit
 fi
